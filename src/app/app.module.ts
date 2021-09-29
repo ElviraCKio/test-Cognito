3e8809a0d28868aa771b782d12fb02efe611f55b
Elvira Camarillo <gcamarillo@kionetworks.com>
2021-09-29 14:28:25 -0500

    ADD project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,6 @@ import { LoginComponent } from './login/login.component';
 import { SingUpComponent } from './sing-up/sing-up.component';
 import { HomeComponent } from './home/home.component';
 
-/* Add Amplify imports */
-import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-import Amplify from 'aws-amplify';
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +15,6 @@ import Amplify from 'aws-amplify';
     HomeComponent
   ],
   imports: [
-    AmplifyUIAngularModule,
     BrowserModule,
     AppRoutingModule
   ],
